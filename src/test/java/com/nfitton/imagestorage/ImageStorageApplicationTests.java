7ee6154c15d30f0777b4c3f974ab727dff0af7ca
nf-PostQuantum <nf@post-quantum.com>
2018-12-29 23:48:48 +0000

    revert file name change

--- src/test/java/com/nfitton/imagestorage/ImageStorageApplicationTests.java
+++ src/test/java/com/nfitton/imagestorage/ImageStorageApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SurveillanceApplicationTests {
+public class ImageStorageApplicationTests {
 
 	@Test
 	public void contextLoads() {
